48479818962b12c1591089a36a186777feb10c17
Lavaugh Brown <lavaugh@usf.edu>
2025-10-16 16:07:42 -0400

    Initial commit of the Mantine project structure

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // The base path should be the project repository name (e.g., /my-app-repo/)
 export default defineConfig({
   plugins: [react()],
-  base: '/your-github-project-name/', // 👈 REPLACE with your repository name!
+  base: '/Wayne-s-Business/', // 👈 REPLACE with your repository name!
 })
